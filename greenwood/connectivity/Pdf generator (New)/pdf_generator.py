5fd170c0d2539017a806aa9fca8805e2fda58b2a
jwlim020921 <45537776+jwlim020921@users.noreply.github.com>
2023-02-24 15:27:52 +0800

    Add files via upload

--- greenwood/connectivity/Pdf generator (New)/pdf_generator.py
+++ greenwood/connectivity/Pdf generator (New)/pdf_generator.py
@@ -1,30 +1,35 @@
 import jinja2
 import pdfkit
-def generatePdf(elephant_frequency:int, motorbike_frequency:int, gunshot_frequency:int, human_frequency:int, 
-                logging_frequency:int, logging_occurences:int, poaching_occurences:int):
+import os
+import sys
+root_directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(root_directory, 'import'))
 
-    elephant_f = elephant_frequency
-    motorbike_f = motorbike_frequency
-    gunshot_f = gunshot_frequency
-    human_f = human_frequency
-    logging_f = logging_frequency
-    logging_o = logging_occurences
-    poaching_o = poaching_occurences
+class pdfgen:
+	def __init__(self, elephant_f, motorbike_f, gunshot_f, human_f, logging_f, logging_o, poaching_o):
+
+		self.elephant_f = elephant_f
+		self.motorbike_f = motorbike_f
+    		self.gunshot_f = gunshot_f
+    		self.human_f = human_f
+    		self.logging_f = logging_f
+    		self.logging_o = logging_o
+    		self.poaching_o = poaching_o
 
 
-    context = {'ef': elephant_f, 'mf': motorbike_f, 
-            'gf': gunshot_f, 'hf': human_f, 'lf' : logging_f, 
-            'lo' : logging_o, 'po' : poaching_o}
+	def pdfgenerator(greenwood):
+		context = {'ef': elephant_f, 'mf': motorbike_f, 'gf': gunshot_f, 'hf': human_f, 'lf' : logging_f, 'lo' : logging_o, 'po' : poaching_o}
 
-    template_loader = jinja2.FileSystemLoader('./')
-    template_env = jinja2.Environment(loader=template_loader)
+    		template_loader = jinja2.FileSystemLoader('./')
+    		template_env = jinja2.Environment(loader=template_loader)
 
-    file_template = r'pdf-template.html'
-    template = template_env.get_template(file_template)
-    output_text = template.render(context)
+    		file_template = r'pdf-template.html'
+    		template = template_env.get_template(file_template)
+    		output_text = template.render(context)
 
-    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
-    pdfkit.from_string(output_text, 'generated_data.pdf',
-    configuration=config, css = 'style.css')
+    		config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    		pdfkit.from_string(output_text, 'generated_data.pdf',
+    		configuration = config, css = 'style.css')
 
-generatePdf(15, 15, 6, 92, 58, 59, 83)
+generatePdf = pdfgen(15, 15, 6, 92, 58, 59, 83)
+generatePdf.self()
